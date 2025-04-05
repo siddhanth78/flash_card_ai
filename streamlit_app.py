a11e330b71622a614193d428b810c9844896310a
Sid Lalgowdar <sidlalgowdar@Sids-MacBook-Air.local>
2025-04-04 22:04:33 -0600

    need to finish back and fwd

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,25 @@
 import streamlit as st
 import json
 
-st.title("HI")
-st.write("Hello, world!")
+with open("cards.json", "r", encoding="utf-8") as file:
+    cards_json = json.load(file)
+
+def check_ans(op, ans):
+    if op[0] == ans:
+        st.success(f"Nice! {cards_json['1']['explanation']}")
+    else:
+        st.error(f"Nope! The correct answer is {ans}! {cards_json['1']['explanation']}")
+
+def go_back():
+    pass
+
+def go_fwd():
+    pass
+
+st.title("QnA")
+opt = st.radio(f"1. {cards_json['1']['question']}", cards_json['1']['options'])
+
+left, center, right = st.columns(3)
+left.button("Prev", on_click=go_back)
+center.button("Submit", on_click=check_ans, args=(opt, cards_json['1']['answer']), type="primary")
+right.button("Next", on_click=go_fwd)
